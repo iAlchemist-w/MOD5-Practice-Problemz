c52e27f422e374deee7494792851e6312fb00d15
Ryan <ryanhughes2@student.olympic.edu>
2025-02-05 11:34:27 -0800

    Add files via upload
    Misspellings are intention to differentiate python attempts. Funny named files are intentional as well.

---
+++ Loop from 2-20 even numberz.py
@@ -0,0 +1,4 @@
+# Write your code here :-)
+#3. Use a for loop and the range() function to print all even numbers from 2 to 20.
+for number in range(2, 21, 2):
+    print(number)
---
+++ My string OC.py
@@ -0,0 +1,5 @@
+# Write your code here :-)
+#2. Define a string variable (e.g., my_string = "Olympic College"). Use a for loop to print each character on its own line. Convert each character to uppercase before printing.
+my_string = "Olympic College"
+for char in my_string:
+    print(char.upper())
