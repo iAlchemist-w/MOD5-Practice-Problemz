e3d333c64d62437fad34eb94c1095133665a9807
Ryan <ryanhughes2@student.olympic.edu>
2025-02-10 14:24:12 -0800

    Delete mod5 positive interger.py

--- mod5 positive interger.py
+++
@@ -1,9 +0,0 @@
-# Write your code here :-)
-#1. Prompt the user for a positive integer n. Use a while loop to sum all the integers from 1 up to n. Print the final sum.
-n = int(input("Enter a positive integer: "))
-total_sum = 0
-i = 1
-while i <= n:
-    total_sum += i
-    i += 1
-print(total_sum)
